dc2eb61be737fe8d45c64738b4bf09fff2c1eb69
Daniel Heredia <rorhedia@gmail.com>
2020-08-25 00:44:21 -0500

    Creación de archivos scss

--- Components/Button/index.js
+++ Components/Button/index.js
@@ -1,35 +1,9 @@
-function Button({ type, btnStyle, children }) {
+export default function Button({ type, btnStyle, children }) {
   return (
     <div>
       <button type={type} className={btnStyle}>
         {children}
       </button>
-      <style jsx>
-        {`
-          .btn-orange {
-            color: #ffffff;
-            background-color: #f29e00;
-            border-radius: 5px;
-            border: none;
-            min-width: 104px;
-            height: 35.18px;
-            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.4),
-              0 6px 20px 0 rgba(0, 0, 0, 0.19);
-          }
-          .btn-green {
-            color: #ffffff;
-            background-color: #9cbf01;
-            border-radius: 5px;
-            border: none;
-            min-width: 104px;
-            height: 35.18px;
-            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.4),
-              0 6px 20px 0 rgba(0, 0, 0, 0.19);
-          }
-        `}
-      </style>
     </div>
   );
 }
-
-export default Button;
--- Components/CustomInput/index.js
+++ Components/CustomInput/index.js
@@ -5,23 +5,6 @@ function CustomInput({ type, placeholder, label, name }) {
         <label>{label}</label>
         <input type={type || "text"} name={name} placeholder={placeholder} />
       </div>
-      <style>
-        {`
-        .input-container{
-          margin:5px;
-          color:white;
-        }
-        label{
-          display:block;
-          margin-bottom: 5px;
-        }
-        input{
-          padding:8px;
-          border-radius:10px;
-          border:none;
-        }
-        `}
-      </style>
     </div>
   );
 }
