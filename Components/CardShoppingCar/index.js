2f189bbfa826b91168b1ebc61b224c606f28d160
Iliana-Maldonado <nana.malibily@gmail.com>
2020-08-25 17:44:49 -0500

    CardShoppingCar

--- Components/CardShoppingCar/index.js
+++ Components/CardShoppingCar/index.js
@@ -1,23 +1,38 @@
-// import "./CardShoppingCar.css";
-import { Card } from 'antd';
+// import "../../pages/CardShoppingCar.css;
+import { Card, Typography, Col, Row, Space } from 'antd';
 
-const { Meta } = Card
+const { Title, Text } = Typography
 
 function CardShoppingCar () {
   return (
     <div>
-      <Card
-        hoverable
-        style={{ width: 240 }}
-        cover={
-          <img
-            alt='example'
-            src='https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png'
-          />
-        }
-      >
-        <Meta title='Europe Street beat' description='www.instagram.com' />
-      </Card>
+      <div>
+        <Card hoverable style={{ width: 300, height: 200 }}>
+          <Row>
+            <Col span={12}>
+              <img
+                className='imageCardCar'
+                width={120}
+                height={95}
+                src='https://camposdeazahar.es/wp-content/uploads/2019/02/puedo-comer-naranja-si-soy-diabetico-campos-de-azahar.jpg'
+              />
+            </Col>
+            <Col span={12}>
+              <Title level={5}>Naranjas Navalentes</Title>
+              <Text type='secondary'>Fran Frutas y verduras</Text>
+            </Col>
+          </Row>
+          <hr />
+          <div className='space-align-container'>
+            <div className='space-align-block'>
+              <Space align='center'>
+                <br />
+                <p>10 MXN / Kg x 50Kg Total: 500 MXN</p>
+              </Space>
+            </div>
+          </div>
+        </Card>
+      </div>
     </div>
   )
 }
