537aa56c2a897472598e7c0bdb5f75b1f6ebb7f3
Daniel Heredia <rorhedia@gmail.com>
2020-08-26 17:31:51 -0500

    Agregación de evento onclick para desplegar el menu de categorias

--- Components/Categories/index.js
+++ Components/Categories/index.js
@@ -1,7 +1,13 @@
-import Link from "next/link";
-import { Row, Col } from "antd";
+import React, { useState, useEffect } from "react";
+import { MenuOutlined } from "@ant-design/icons";
+
+import Category from "./Category";
+import CustomButton from "../../Components/CustomButton";
 
 export default function Categories() {
+  const [menu, showMenu] = useState(false);
+  const [size, getSize] = useState(0);
+
   const arrowLeft = "images/flecha-izquierda.png";
   const fruits = "images/frutas.png";
   const vegetables = "images/verduras.png";
@@ -9,96 +15,108 @@ export default function Categories() {
   const flowers = "images/flores.png";
   const roots = "images/raices.png";
 
+  useEffect(() => {
+    getSize(window.innerWidth);
+  }, []);
+
+  const show = (elem) => {
+    elem.style.display = "block"; // Make it visible
+    let height = elem.scrollHeight + 90 + "px"; // Get it's height
+    elem.style.display = ""; //  Hide it again
+
+    elem.classList.add("is-visible"); // Make the element visible
+    elem.style.height = height; // Update the max-height
+
+    window.setTimeout(function () {
+      elem.style.height = height;
+    }, 1100);
+  };
+
+  const hide = (elem) => {
+    // Give the element a height to change from
+    elem.style.height = elem.scrollHeight + "px";
+
+    // Set the height back to 0
+    window.setTimeout(function () {
+      elem.style.height = "0";
+    }, 1);
+
+    window.setTimeout(function () {
+      elem.classList.remove("is-visible");
+    }, 350);
+  };
+
+  const showDesktop = (elem) => {
+    let menu = document.querySelector(".categories-container");
+
+    elem.style.display = "block"; // Make it visible
+    let height = elem.scrollHeight + "px"; // Get it's height
+    elem.style.display = ""; //  Hide it again
+    elem.classList.add("is-visible"); // Make the element visible
+    elem.style.height = height; // Update the max-height
+
+    menu.classList.add("is-visible"); // Make the element visible
+
+    window.setTimeout(function () {
+      elem.style.height = height;
+    }, 360);
+  };
+
+  const hideDesktop = (elem) => {
+    let menu = document.querySelector(".categories-container");
+
+    elem.classList.remove("is-visible");
+    menu.classList.remove("is-visible");
+  };
+
+  const handleMenu = () => {
+    const mobileContainer = document.querySelector(".categories-container");
+    const desktopContainer = document.querySelector(".parent-container");
+
+    if (size < 576) {
+      !menu ? show(mobileContainer) : hide(mobileContainer);
+    } else {
+      !menu ? showDesktop(desktopContainer) : hideDesktop(desktopContainer);
+    }
+
+    showMenu(!menu);
+  };
+
   return (
-    <div className="parent-container">
-      <div className="categories-container">
-        <img src={arrowLeft} height="20" width="20" />
+    <>
+      <CustomButton
+        icon={<MenuOutlined />}
+        btnStyle="btn-orange"
+        callback={handleMenu}
+      >
+        Categorías
+      </CustomButton>
 
-        <Row className="mw first">
-          <Col className="categories-img" xs={6} sm={12}>
-            <Link href="/home">
-              <a>
-                <img src={fruits} height="40" width="40" />
-              </a>
-            </Link>
-          </Col>
-          <Col className="categories-name" xs={18} sm={12}>
-            <Link href="/home">
-              <a>
-                <h3>Frutas</h3>
-              </a>
-            </Link>
-          </Col>
-        </Row>
+      <div className="parent-container">
+        <div className="categories-container">
+          <img src={arrowLeft} height="25" width="25" onClick={handleMenu} />
 
-        <Row className="mw">
-          <Col className="categories-img" xs={6} sm={12}>
-            <Link href="/home">
-              <a>
-                <img src={vegetables} height="40" width="40" />
-              </a>
-            </Link>
-          </Col>
-          <Col className="categories-name" xs={18} sm={12}>
-            <Link href="/home">
-              <a>
-                <h3>Verduras</h3>
-              </a>
-            </Link>
-          </Col>
-        </Row>
+          <Category imgPath={fruits} redirect="/home" rowStyle="mw first">
+            Frutas
+          </Category>
 
-        <Row className="mw">
-          <Col className="categories-img" xs={6} sm={12}>
-            <Link href="/home">
-              <a>
-                <img src={pods} height="40" width="40" />
-              </a>
-            </Link>
-          </Col>
-          <Col className="categories-name" xs={18} sm={12}>
-            <Link href="/home">
-              <a>
-                <h3>Vainas</h3>
-              </a>
-            </Link>
-          </Col>
-        </Row>
+          <Category imgPath={vegetables} redirect="/home" rowStyle="mw">
+            Verduras
+          </Category>
 
-        <Row className="mw">
-          <Col className="categories-img" xs={6} sm={12}>
-            <Link href="/home">
-              <a>
-                <img src={flowers} height="40" width="40" />
-              </a>
-            </Link>
-          </Col>
-          <Col className="categories-name" xs={18} sm={12}>
-            <Link href="/home">
-              <a>
-                <h3>Flores</h3>
-              </a>
-            </Link>
-          </Col>
-        </Row>
+          <Category imgPath={pods} redirect="/home" rowStyle="mw">
+            Vainas
+          </Category>
 
-        <Row className="mw last">
-          <Col className="categories-img" xs={6} sm={12}>
-            <Link href="/home">
-              <a>
-                <img src={roots} height="40" width="40" />
-              </a>
-            </Link>
-          </Col>
-          <Col className="categories-name" xs={18} sm={12}>
-            <Link href="/home">
-              <a>
-                <h3>Raíces</h3>
-              </a>
-            </Link>
-          </Col>
-        </Row>
+          <Category imgPath={flowers} redirect="/home" rowStyle="mw">
+            Flores
+          </Category>
+
+          <Category imgPath={roots} redirect="/home" rowStyle="mw last">
+            Raíces
+          </Category>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
