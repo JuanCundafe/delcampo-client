c927189380df197d16c466fe80921612b5dddb8b
Daniel Heredia <rorhedia@gmail.com>
2020-08-26 00:02:13 -0500

    reset

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,3 @@
-import CardAddress from "../Components/CardAddress";
-
 export default function Home() {
-  return (
-    <>
-      <CardAddress
-        phone="9999999999"
-        email="email@gmail.com"
-        address="Tlaxiaco, Oaxaca"
-        title="Contacto"
-        btnSelect={true}
-      />
-    </>
-  );
+  return <></>;
 }
