b386232f257ef3328b10d871981511ff712dd5ff
Iliana-Maldonado <nana.malibily@gmail.com>
2020-08-28 02:54:08 -0500

    screen register

--- pages/register/index.jsx
+++ pages/register/index.jsx
@@ -1,63 +1,135 @@
-import { Input, Radio, Checkbox, Row, Col, Typography } from "antd";
-import CustomButton from "../../Components/CustomButton";
-import Navbar from "../../Components/Navbar";
+import {
+  Layout,
+  Anchor,
+  Row,
+  Col,
+  Form,
+  Input,
+  Button,
+  Radio,
+  Checkbox,
+} from "antd";
 
-const App = () => (
-  <Radio.Group size="medium" name="radiogroup" defaultValue={1}>
-    <Radio value={1}>Productor</Radio>
-    <Radio value={2}>Comprador</Radio>
-  </Radio.Group>
-);
+const { Header, Content } = Layout;
+const { Link } = Anchor;
 
-const { Link } = Typography;
-
-function onChange(e) {
-  console.log(`checked = ${e.target.checked}`);
-}
+import { UserOutlined, LockOutlined, MailOutlined } from "@ant-design/icons";
 
 export default function Register() {
+  const onFinish = (values) => {
+    console.log("Received values of form: ", values);
+  };
+
   return (
-    <div className="register-background">
-      <Navbar />
-      <div className="register-container">
-        <div className="register-title">
-          <h2>Regístrate</h2>
-        </div>
-        <div className="register-body">
-          <Row>
-            <Col span={20} offset={2}>
-              <p>Nombre:</p>
-              <Input gray-5 placeholder="" />
-              <p>Correo:</p>
-              <Input gray-5 placeholder="" />
-              <p>Contraseña:</p>
-              <Input gray-5 placeholder="" />
-              <p>Repetir Contraseña:</p>
-              <Input gray-5 placeholder="" />
-            </Col>
-          </Row>
-          <Row justify="center">
-            <Col span={20} offset={4}>
-              <App />
-            </Col>
-          </Row>
-          <Row>
-            <Col span={20} offset={4}>
-              <Checkbox onChange={onChange}>Términos y Condiciones</Checkbox>
-            </Col>
-          </Row>
-        </div>
-        <div className="register-button">
-          <CustomButton btnStyle="btn-orange">Aceptar</CustomButton>
-        </div>
-        <Row>
-          <Col span={14} offset={5}>
-            <a href="https://ant.design" target="_blank">
-              ¿Ya tienes una cuenta? Click aqui.
+    <>
+      <div id="register-screen">
+        <Layout>
+          <Header>
+            <a href="/">
+              <img src="images/logo-delcampo.png" alt="Logo del campo" />
             </a>
-          </Col>
-        </Row>
+            <a href="">
+              <Button type="primary" className="register">
+                Registrate
+              </Button>
+            </a>
+          </Header>
+          <Content>
+            <Row className="login-section">
+              <Col className="register-form">
+                <h1>Regístrate</h1>
+                <Form
+                  name="normal_login"
+                  className="login-form"
+                  initialValues={{ remember: true }}
+                  onFinish={onFinish}
+                >
+                  <Form.Item
+                    name="username"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Por favor ingresa tu usuario",
+                      },
+                    ]}
+                  >
+                    <Input
+                      prefix={<UserOutlined className="site-form-item-icon" />}
+                      placeholder="Username"
+                    />
+                  </Form.Item>
+                  <Form.Item
+                    name="mail"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Por favor ingresa tu correo",
+                      },
+                    ]}
+                  >
+                    <Input
+                      prefix={<MailOutlined className="site-form-item-icon" />}
+                      placeholder="Mail"
+                    />
+                  </Form.Item>
+                  <Form.Item
+                    name="password"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Por favor ingresa tu contraseña",
+                      },
+                    ]}
+                  >
+                    <Input
+                      prefix={<LockOutlined className="site-form-item-icon" />}
+                      type="password"
+                      placeholder="Password"
+                    />
+                  </Form.Item>
+                  <Form.Item
+                    name="repeat-password"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Por favor ingresa nuevamente tu contraseña",
+                      },
+                    ]}
+                  >
+                    <Input
+                      prefix={<LockOutlined className="site-form-item-icon" />}
+                      type="password"
+                      placeholder="Repeat Password"
+                    />
+                  </Form.Item>
+                  <Radio.Group>
+                    <Radio value={1}>Productor</Radio>
+                    <Radio value={2}>Comprador</Radio>
+                  </Radio.Group>
+                  <Form.Item>
+                    <Checkbox>Términos y Condiciones</Checkbox>
+                  </Form.Item>
+                  <Form.Item>
+                    <Button
+                      type="primary"
+                      htmlType="submit"
+                      className="login-form-button"
+                    >
+                      Aceptar
+                    </Button>
+                  </Form.Item>
+                  <Form.Item>
+                    <a>Ya tienes una cuenta? Click Aquí</a>
+                  </Form.Item>
+                </Form>
+              </Col>
+              <Col className="img-welcome">
+                <img src="images/welcome-register.png" alt="Logo del campo" />
+              </Col>
+            </Row>
+          </Content>
+        </Layout>
       </div>
-    </div>
+    </>
   );
 }
