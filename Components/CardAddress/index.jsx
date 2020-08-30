81c67c042571b2936dec9b3be85ee447ef93860d
Juan_Cundafé <juan.cundafe@crowdsense.app>
2020-08-30 18:05:48 -0500

    avance mal en mi rama lo de shipping

--- Components/CardAddress/index.jsx
+++ Components/CardAddress/index.jsx
@@ -1,44 +1,51 @@
-import CustomButton from '../CustomButton';
+import CustomButton from "../CustomButton";
+import { Row, Col } from "antd";
 
-export default function CardAddress ({
+export default function CardAddress({
   phone,
   email,
   address,
   title,
-  btnSelect
+  btnSelect,
 }) {
-  const imgPhone = 'images/phone.jpg';
-  const imgEmail = 'images/email.jpg';
-  const imgAddress = 'images/address.jpg';
+  const imgPhone = "images/phone.jpg";
+  const imgEmail = "images/email.jpg";
+  const imgAddress = "images/address.jpg";
 
   return (
-    <div className='card-address-container'>
-      <div className='card-address-header'>
+    <div className="card-address-container">
+      <div className="card-address-header">
         <h3>{title}</h3>
       </div>
-      <div className='card-address-body'>
-        <div className='contact-information'>
-          <img src={imgPhone} width='20' height='20' />
-          {phone}
-        </div>
-        <div className='contact-information'>
-          <img src={imgEmail} width='20' height='20' />
-          {email}
-        </div>
-        <div className='contact-information'>
-          <img src={imgAddress} width='15' height='20' />
-          <p>{address}</p>
-        </div>
+      <div className="card-address-body">
+        <Row className="contact-information">
+          <Col span={4}>
+            <img src={imgPhone} width="20" height="20" />
+          </Col>
+          <Col span={4}>{phone}</Col>
+        </Row>
+        <Row className="contact-information">
+          <Col>
+            <img src={imgEmail} width="20" height="20" />
+          </Col>
+          <Col>{email}</Col>
+        </Row>
+        <Row className="contact-information">
+          <Col>
+            <img src={imgAddress} width="15" height="20" />
+          </Col>
+          <Col>{address}</Col>
+        </Row>
 
-        <div className='card-button-container'>
+        <div className="card-button-container">
           {btnSelect ? (
-            <CustomButton btnStyle='btn-green'>Seleccionar</CustomButton>
+            <CustomButton btnStyle="btn-green">Seleccionar</CustomButton>
           ) : null}
-          <CustomButton btnStyle='btn-green'>Editar</CustomButton>
+          <CustomButton btnStyle="btn-green">Editar</CustomButton>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 {
