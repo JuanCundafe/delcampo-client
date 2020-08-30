3a863967b42862c893a0f614892fb89f9f83a62c
Juan_Cundafé <juan.cundafe@crowdsense.app>
2020-08-29 21:45:45 -0500

    landing lista funcionando cargando de back

--- Components/CardHarvest/index.jsx
+++ Components/CardHarvest/index.jsx
@@ -1,42 +1,41 @@
-import { Card, Avatar, Row, Col } from "antd";
+import { Card, Avatar, Row, Col } from 'antd';
 
-export default function CardHarvest({
+export default function CardHarvest ({
   product,
   price,
   description,
   picture,
-  weight,
+  weight
 }) {
-  const { Meta } = Card;
-
+  const { Meta } = Card
   return (
     <>
-      <div className="wrapper-cardHarvest">
+      <div className='wrapper-cardHarvest'>
         <Card
           hoverable
-          className="card-harvest"
+          className='card-harvest'
           cover={
-            <img className="cardHarvest-img" alt="example" src={picture} />
+            <img className='cardHarvest-img' alt='example' src={picture} />
           }
         >
           <Meta
-            className="meta-cardHarvest module overflow"
+            className='meta-cardHarvest module overflow'
             title={product}
             description={description}
           />
-          <Row className="productor-cardHarvest">
+          <Row className='productor-cardHarvest'>
             <Avatar
-              className="avatar-cardHarvest"
-              src="images/avatar-test.png"
+              className='avatar-cardHarvest'
+              src='images/avatar-test.png'
             />
-            <Meta description="Frutas y verduas" />
+            <Meta description='Frutas y verduas' />
             <Col
               xs={{ offset: 1 }}
               lg={{ offset: 10 }}
-              className="col-precio-cardHarvest"
+              className='col-precio-cardHarvest'
             >
               <Meta
-                className="precio-cardHarvest"
+                className='precio-cardHarvest'
                 description={`${price} mxn / Kg`}
               />
             </Col>
@@ -44,5 +43,5 @@ export default function CardHarvest({
         </Card>
       </div>
     </>
-  );
+  )
 }
