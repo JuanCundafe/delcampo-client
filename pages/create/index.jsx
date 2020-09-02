a0407b1629099c6542c8c7f944f2232b5ad8e863
codigoVis <etneciv2@icloud.com>
2020-09-02 14:44:08 -0500

    perdi mis cambios

--- pages/create/index.jsx
+++ pages/create/index.jsx
@@ -1,17 +1,17 @@
-import { useState, useEffect } from "react";
-import ImageUploading from "react-images-uploading";
+import { useState, useEffect } from 'react';
+import ImageUploading from 'react-images-uploading';
 
-import Navbar from "../../Components/Navbar";
-import MenuFooter from "../../Components/MenuFooter";
-import CustomButton from "../../Components/CustomButton";
-import InputKilograms from "../../Components/InputKilograms";
-import UploadProduct from "./components/UploadProduct";
+import Navbar from '../../Components/Navbar';
+import MenuFooter from '../../Components/MenuFooter';
+import CustomButton from '../../Components/CustomButton';
+import InputKilograms from '../../Components/InputKilograms';
+import UploadProduct from './components/UploadProduct';
 import {
   GetProduct,
   addHarvest,
   imageHarvest,
-  getHarvest,
-} from "../../lib/services";
+  getHarvest
+} from '../../lib/services';
 
 import {
   Layout,
@@ -21,17 +21,17 @@ import {
   DatePicker,
   Select,
   Form,
-  Button,
-} from "antd";
-const { Content } = Layout;
-const { TextArea } = Input;
+  Button
+} from 'antd';
+const { Content } = Layout
+const { TextArea } = Input
 
-export default function Create() {
-  const [product, setProduct] = useState(false);
-  const [result, setResult] = useState([]);
-  const [images, setImages] = React.useState([]);
-  const [harvestProduct, setHarvestProduct] = useState({});
-  const { Option } = Select;
+export default function Create () {
+  const [product, setProduct] = useState(false)
+  const [result, setResult] = useState([])
+  const [images, setImages] = React.useState([])
+  const [harvestProduct, setHarvestProduct] = useState({})
+  const { Option } = Select
 
   // function handleChangeProduct (value) {
   //   console.log(`selected ${value}`)
@@ -39,46 +39,46 @@ export default function Create() {
 
   const onChange = (imageList, addUpdateIndex) => {
     // data for submit
-    console.log(imageList, addUpdateIndex);
-    setImages(imageList);
+    console.log(imageList, addUpdateIndex)
+    setImages(imageList)
   };
 
   useEffect(() => {
-    async function fetchproduc() {
-      const token = localStorage.getItem("token");
+    async function fetchproduc () {
+      const token = localStorage.getItem('token')
       try {
         const response = await GetProduct(
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmNGIwM2MxMmUwMWZhYmEwZmU5Y2EzNiIsImlhdCI6MTU5OTAzMzUwNCwiZXhwIjoxNTk5MjA2MzA0fQ.w-EDRLmEAu1s2ez2Q2UH7Y8D-4KGwIeXtVf2Hf9rNNk"
-        );
-        const { data } = response;
-        const { products } = data;
-        setResult(products);
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmNGIwM2MxMmUwMWZhYmEwZmU5Y2EzNiIsImlhdCI6MTU5OTAzMzUwNCwiZXhwIjoxNTk5MjA2MzA0fQ.w-EDRLmEAu1s2ez2Q2UH7Y8D-4KGwIeXtVf2Hf9rNNk'
+        )
+        const { data } = response
+        const { products } = data
+        setResult(products)
       } catch (error) {}
     }
-    fetchproduc();
-  }, []);
+    fetchproduc()
+  }, [])
   useEffect(() => {
-    async function fetchHarvest() {
+    async function fetchHarvest () {
       try {
-        const response = await getHarvest();
-        const { dataHarvest } = response;
-        const { harvestProduct } = dataHarvest;
-        setResult(harvestProduct);
+        const response = await getHarvest()
+        const { dataHarvest } = response
+        const { harvestProduct } = dataHarvest
+        setResult(harvestProduct)
       } catch (error) {}
     }
-    fetchHarvest();
-  }, []);
+    fetchHarvest()
+  }, [])
 
   const handelFormProduct = () => {
-    setProduct(true);
+    setProduct(true)
   };
 
   const handeleTotal = (total) => {
-    console.log(total);
+    console.log(total)
   };
 
   const onFinish = async (values) => {
-    console.log(values);
+    console.log(values)
     // try {
     //   const response = await addHarvest(){
 
@@ -90,22 +90,22 @@ export default function Create() {
     <div>
       <Layout>
         <Navbar />
-        <Content className="container-create">
+        <Content className='container-create'>
           <Row>
             <Col xs={{ span: 20, offset: 2 }} md={{ span: 16, offset: 4 }}>
               <p>Agrega una foto interesante de tu cosecha</p>
-              <div className="uploadImage">
+              <div className='uploadImage'>
                 <h1>Upload image</h1>
 
-                <div className="App">
+                <div className='App'>
                   <ImageUploading
                     value={images}
                     onChange={onChange}
                     maxNumber={1}
-                    dataURLKey="data_url"
+                    dataURLKey='data_url'
                   >
                     {({ imageList, onImageUpload, onImageRemoveAll }) => (
-                      <div className="upload__image-wrapper">
+                      <div className='upload__image-wrapper'>
                         <CustomButton callback={onImageUpload}>
                           Click
                         </CustomButton>
@@ -114,12 +114,12 @@ export default function Create() {
                           Remove all images
                         </button>
                         {imageList.map((image, index) => (
-                          <div key={index} className="image-item">
+                          <div key={index} className='image-item'>
                             <img
                               src={image.data_url}
-                              alt=""
-                              width="300"
-                              height="200"
+                              alt=''
+                              width='300'
+                              height='200'
                             />
                           </div>
                         ))}
@@ -144,8 +144,8 @@ export default function Create() {
                 ))}
               </Select> */}
 
-                <Form.Item name="Product">
-                  <Select placeholder="Selecciona un Producto">
+                <Form.Item name='Product'>
+                  <Select placeholder='Selecciona un Producto'>
                     {result.map(({ _id, name }) => (
                       <Option value={_id} key={_id}>
                         {name}
@@ -155,23 +155,23 @@ export default function Create() {
                 </Form.Item>
 
                 <p>Describe tu cosecha:</p>
-                <Form.Item name="description">
+                <Form.Item name='description'>
                   <Input.TextArea />
                 </Form.Item>
-                {/* <TextArea rows={6} /> */}
+                {/* <TextArea rows={6} c/> */}
 
                 <p>Precio por kilogramo</p>
                 <div
                   style={{
                     marginBottom: 16,
-                    width: "70%",
+                    width: '70%'
                   }}
                 >
-                  <Form.Item name="price">
+                  <Form.Item name='price'>
                     <Input
-                      className="kilogramos"
-                      addonBefore="$"
-                      addonAfter="Kg"
+                      className='kilogramos'
+                      addonBefore='$'
+                      addonAfter='Kg'
                     />
                   </Form.Item>
                 </div>
@@ -180,9 +180,9 @@ export default function Create() {
                 <InputKilograms callback={handeleTotal} />
 
                 <p>Fecha límite de venta de la cosecha</p>
-                <Form.Item name="date_end">
+                <Form.Item name='date_end'>
                   <DatePicker
-                    style={{ background: "#dfdfe3", width: "100%" }}
+                    style={{ background: '#dfdfe3', width: '100%' }}
                   />
                 </Form.Item>
 
@@ -192,7 +192,7 @@ export default function Create() {
                 <br />
 
                 <Form.Item>
-                  <Button type="primary" htmlType="submit">
+                  <Button type='primary' htmlType='submit'>
                     Agregar Cosecha
                   </Button>
                 </Form.Item>
@@ -203,5 +203,5 @@ export default function Create() {
         <MenuFooter />
       </Layout>
     </div>
-  );
+  )
 }
