106e9fc3ad23837f76f9cbd9b5e0535f052e3851
codigoVis <etneciv2@icloud.com>
2020-09-02 00:04:30 -0500

    s3

--- Components/CustomSelect/index.jsx
+++ Components/CustomSelect/index.jsx
@@ -1,31 +1,31 @@
-import { Select, Row, Col } from "antd";
+import { Select, Row, Col } from 'antd';
 
-const { Option } = Select;
-export default function InputKilograms({ callback }) {
-  function handleChange(value) {
-    console.log(`selected ${value}`);
-    callback(value);
+const { Option } = Select
+export default function InputKilograms ({ callback }) {
+  function handleChange (value) {
+    console.log(`selected ${value}`)
+    callback(value)
   }
   return (
     <>
       <Row>
         <Col span={12} xs={24} sm={12}>
-          <div className="container-kilograms">
+          <div className='container-kilograms'>
             <Select
-              name="category"
-              className="custo-select"
-              defaultValue=""
+              name='category'
+              className='custo-select'
+              defaultValue=''
               onChange={handleChange}
             >
-              <Option value="Frutas">Frutas</Option>
-              <Option value="Vainas">Vainas</Option>
-              <Option value="Flores">Flores</Option>
-              <Option value="Verduras">Verduras</Option>
-              <Option value="Raíces">Raices</Option>
+              <Option value='Frutas'>Frutas</Option>
+              <Option value='Vainas'>Vainas</Option>
+              <Option value='Flores'>Flores</Option>
+              <Option value='Verduras'>Verduras</Option>
+              <Option value='Raíces'>Raices</Option>
             </Select>
           </div>
         </Col>
       </Row>
     </>
-  );
+  )
 }
--- pages/create/index.jsx
+++ pages/create/index.jsx
@@ -1,21 +1,64 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import ImageUploading from "react-images-uploading";
 
-import { Layout, Upload, message, Col, Row, Input, DatePicker } from "antd";
+import {
+  Layout,
+  Upload,
+  message,
+  Col,
+  Row,
+  Input,
+  DatePicker,
+  Select,
+  Form,
+} from "antd";
 
 import Navbar from "../../Components/Navbar";
 import MenuFooter from "../../Components/MenuFooter";
 import CustomButton from "../../Components/CustomButton";
 import InputKilograms from "../../Components/InputKilograms";
 import UploadProduct from "./components/UploadProduct";
+import { GetProduct } from "../../lib/services";
+import { addHarvestId } from "../../lib/services";
 
 const { Content } = Layout;
 const { TextArea } = Input;
 
 export default function Create() {
   const [product, setProduct] = useState(false);
+  const [result, setResult] = useState([]);
+  const [images, setImages] = React.useState([]);
+  const maxNumber = 69;
+  const { Option } = Select;
+
+  function handleChange(value) {
+    console.log(`selected ${value}`);
+  }
+  const onChange = (imageList, addUpdateIndex) => {
+    // data for submit
+    console.log(imageList, addUpdateIndex);
+    setImages(imageList);
+  };
+  useEffect(() => {
+    async function fetchproduc() {
+      const viz =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmNDliN2Y3YjE1MjI3MDA3ZTA5NTA4NyIsImlhdCI6MTU5ODg5ODMxMCwiZXhwIjoxNTk5MDcxMTEwfQ.dTgbhZJTyDMPHkhd4E65FpNHoDkt552CoZBIiYS0LLc";
+      try {
+        const response = await GetProduct(viz);
+        const { data } = response;
+        const { products } = data;
+        console.log(data);
+        setResult(products);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchproduc();
+  }, []);
   const handelFormProduct = () => {
     setProduct(true);
   };
+
   return (
     <div>
       <Layout>
@@ -24,13 +67,84 @@ export default function Create() {
           <Row>
             <Col xs={{ span: 20, offset: 2 }} md={{ span: 16, offset: 4 }}>
               <p>Agrega una foto interesante de tu cosecha</p>
-              <div className="uploadImage">
-                <h1>Upload image</h1>
-              </div>
+              <Form onSubmit={processHarvest}>
+                <div className="uploadImage">
+                  <h1>Upload image</h1>
+                  <div className="App">
+                    <ImageUploading
+                      multiple
+                      value={images}
+                      onChange={onChange}
+                      maxNumber={1}
+                      dataURLKey="data_url"
+                    >
+                      {({
+                        imageList,
+                        onImageUpload,
+                        onImageRemoveAll,
+                        onImageUpdate,
+                        onImageRemove,
+                        isDragging,
+                        dragProps,
+                      }) => (
+                        // write your building UI
+                        <div className="upload__image-wrapper">
+                          {/* <button
+                          style={isDragging ? { color: "red" } : undefined}
+                          onClick={onImageUpload}
+                          {...dragProps}
+                        ></button> */}
+                          <CustomButton callback={onImageUpload}>
+                            Click or Drop here
+                          </CustomButton>
+                          &nbsp;
+                          <button onClick={onImageRemoveAll}>
+                            Remove all images
+                          </button>
+                          {imageList.map((image, index) => (
+                            <div key={index} className="image-item">
+                              <img
+                                src={image["data_url"]}
+                                alt=""
+                                width="300"
+                                height="200"
+                              />
+                              {/* <div className="image-item__btn-wrapper">
+                              <button onClick={() => onImageUpdate(index)}>
+                                Update
+                              </button>
+                              <button onClick={() => onImageRemove(index)}>
+                                Remove
+                              </button>
+                            </div> */}
+                            </div>
+                          ))}
+                        </div>
+                      )}
+                    </ImageUploading>
+                  </div>
+                  ); }
+                </div>
+              </Form>
+              {({ imageList, onImageUpload, onImageRemoveAll, errors }) => (
+                <div>
+                  {errors.maxNumber && (
+                    <span>Number of selected images exceed maxNumber</span>
+                  )}
+                </div>
+              )}
+
               <CustomButton callback={handelFormProduct}>
                 + Agregar Producto
               </CustomButton>
               {product && <UploadProduct />}
+              <Select defaultValue="Uvas" onChange={handleChange}>
+                {result.map((product) => (
+                  <Option value={product.name} key={product._id}>
+                    {product.name}
+                  </Option>
+                ))}
+              </Select>
               <p>Describe tu cosecha:</p>
               <TextArea rows={6} />
               <p>Precio por kilogramo</p>
